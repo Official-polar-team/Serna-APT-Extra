92655eb809dbae8e190608ac38bd37e7366fc852
Jon Doe <john@bugmenot.com>
2019-09-02 17:16:57 -0700

    Update packme.sh

--- packme.sh
+++ packme.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh 
 
-apt_extra_version=0.0.4 #Change me every release
+apt_extra_version=0.0.5 #Change me every release
 script_full_path=$(dirname "$0")
 
 cd $script_full_path/add-apt-repositories
